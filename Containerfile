f58e9020d90cbbee4a78c7d9975f3a35e298c085
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2024-11-25 23:39:24 -0300

    project refactor

--- Containerfile
+++ Containerfile
@@ -1,11 +1,11 @@
 ARG IMAGE_SOURCE=quay.io/fedora/fedora-silverblue
 ARG IMAGE_TAG=41
 
-FROM scratch AS ctx
+FROM scratch AS sources
 COPY / /
 
 FROM ${IMAGE_SOURCE}:${IMAGE_TAG}
-RUN --mount=type=cache,dst=/var/cache/rpm-ostree \
-    --mount=type=cache,dst=/var/cache/dnf \
-    --mount=type=bind,from=ctx,src=/,dst=/ctx \
-    cp -r /ctx/rootfs/* / && /ctx/build.sh
+RUN --mount=type=cache,dst=/var \
+    --mount=type=cache,dst=/tmp \
+    --mount=type=bind,from=sources,src=/,dst=/sources \
+    cp -r /sources/rootfs/* / && /sources/build.sh
--- build.sh
+++ build.sh
@@ -2,28 +2,37 @@
 set -eux -o pipefail
 
 ### prepare ###
-tmpdir=$(mktemp -d) && pushd "$tmpdir"
-mkdir -p /var/lib/alternatives
+tmpdir=$(mktemp -d) && cd "$tmpdir"
 
 ### enable repos ###
+cp /etc/yum.repos.d/rpmfusion-nonfree-steam.repo /etc/yum.repos.d/rpmfusion-nonfree-steam.repo.bak
 sed -Ei '0,/^enabled=.*$/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
 
+### support for non-compliant package directories ###
+# requires a 'tmpfiles.d' configuration to symlink specific locations, e.g.:
+# /usr/lib/opt/dir -> /var/opt/dir
+# /usr/lib/usrlocal/dir -> /var/usrlocal/dir
+mkdir -p /usr/lib/{opt,usrlocal}
+ln -sfT /usr/lib/usrlocal /var/usrlocal
+ln -sfT /usr/lib/opt /var/opt
+
 ### install packages ###
-dnf5 install -y \
+dnf install -y \
 	langpacks-{en,pt} \
 	zsh eza bat micro mc \
 	fzf fd-find ripgrep ncdu tldr tmux \
 	btop htop xclip xsel wl-clipboard \
 	iperf3 firewall-config syncthing \
 	distrobox podman{,-compose,-docker,-tui} \
-	btrfs-assistant gparted p7zip{,-plugins} cabextract \
+	btrfs-assistant gparted p7zip{,-plugins} \
 	cups-pdf gnome-themes-extra gnome-tweaks tilix \
 	wireguard-tools \
 	openrgb steam-devices \
 	virt-manager \
 	onedrive python3-pyside6 python3-requests \
 	tailscale 1password-cli \
-	https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+	https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm \
+	https://guardiao.itau.com.br/warsaw/warsaw_setup_fedora_64.rpm
 
 ### install cloudflared ###
 wget -q -O /usr/bin/cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
@@ -33,7 +42,7 @@ chmod +x /usr/bin/cloudflared
 ### install onedrive-gui ###
 git clone --branch=main --depth=1 https://github.com/bpozdena/OneDriveGUI.git /usr/lib/OneDriveGUI
 rm -rf /usr/lib/OneDriveGUI/.git
-ln -s /usr/share/applications/OneDriveGUI.desktop /etc/xdg/autostart/OneDriveGUI.desktop
+ln -sfT /usr/share/applications/OneDriveGUI.desktop /etc/xdg/autostart/OneDriveGUI.desktop
 
 ### configure rpm-ostree ###
 sed -Ei '/AutomaticUpdatePolicy=/c\AutomaticUpdatePolicy=stage' /etc/rpm-ostreed.conf >/dev/null
@@ -68,7 +77,9 @@ systemctl enable tailscaled.service
 sed -Ei 's|(^Exec=gnome-disk-image-mounter\b)|\1 --writable|g' /usr/share/applications/gnome-disk-image-mounter.desktop
 
 ### configure udisks2 ###
-udisks2_generate() { echo "$(grep -Eo "\b$1=.+" /etc/udisks2/mount_options.conf.example | tail -n 1),$2"; }
+udisks2_generate() {
+	({ set +x; } &>/dev/null && echo "$(grep -Eo "\b$1=.+" /etc/udisks2/mount_options.conf.example | tail -n 1),$2")
+}
 cat >/etc/udisks2/mount_options.conf <<-EOF
 	[defaults]
 	$(udisks2_generate 'ntfs_defaults' 'dmask=0022,fmask=0133,noatime')
@@ -76,9 +87,12 @@ cat >/etc/udisks2/mount_options.conf <<-EOF
 	$(udisks2_generate 'vfat_defaults' 'dmask=0022,fmask=0133,noatime')
 EOF
 
+### configure warsaw ###
+systemctl enable warsaw.service
+
 ### cleanup ###
-rm -rf /etc/yum.repos.d/{tailscale,1password}.repo /var/log/dnf*
-popd && rm -rf "$tmpdir"
+mv -f /etc/yum.repos.d/rpmfusion-nonfree-steam.repo.bak /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
+rm -rf /etc/yum.repos.d/{tailscale,1password}.repo
 
 ### commit ###
 ostree container commit
